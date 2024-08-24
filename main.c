cb0d6f09dc5d5beb61d56c7aed07dd63c6d13791
pi55man <cuinnv@gmail.com>
2024-08-24 23:57:55 +0530

    BASIC heap allocation

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <assert.h>
+#define HEAP_CAP 64000
+
+
+char heap[HEAP_CAP];
+size_t heap_size = 0;
+void* heap_alloc(size_t size)
+{	
+	assert(heap_size+size<=HEAP_CAP);
+	void* result = heap + heap_size;
+	heap_size+=size;
+	return result;
+}
+
+
 
 
 int main(){
 
-
-	return 0;
+	return 0; 
 }
